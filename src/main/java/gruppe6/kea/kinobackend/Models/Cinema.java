f999bb98dea0bebbef0c68ade6cb6f42955c6bdd
gustavtjac <gustavtorup@gmail.com>
2025-09-29 14:01:23 +0200

    Cinema Model lavet

--- src/main/java/gruppe6/kea/kinobackend/Models/Cinema.java
+++ src/main/java/gruppe6/kea/kinobackend/Models/Cinema.java
@@ -1,19 +1,61 @@
 package gruppe6.kea.kinobackend.Models;
 
+import gruppe6.kea.kinobackend.Models.Enums.Authlevel;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import org.springframework.data.repository.cdi.Eager;
 
 @Entity
 public class Cinema {
-    @Id
-    private Long id;
 
-    public void setId(Long id) {
+   @Id
+    private int id;
+
+   @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private Authlevel authlevel;
+
+    public Cinema() {
+    }
+
+    public Cinema(String username, String password, Authlevel authlevel) {
+        this.username = username;
+        this.password = password;
+        this.authlevel = authlevel;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
         this.id = id;
     }
 
-    public Long getId() {
-        return id;
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Authlevel getAuthlevel() {
+        return authlevel;
+    }
+
+    public void setAuthlevel(Authlevel authlevel) {
+        this.authlevel = authlevel;
     }
 }
