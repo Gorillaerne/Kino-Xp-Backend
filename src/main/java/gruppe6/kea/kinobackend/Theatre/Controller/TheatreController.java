48fe8efa9b5e6724039f8718000a6801b3f61633
gustavtjac <gustavtorup@gmail.com>
2025-09-30 13:54:34 +0200

    CORS PÅ ALT!!!!

--- src/main/java/gruppe6/kea/kinobackend/Theatre/Controller/TheatreController.java
+++ src/main/java/gruppe6/kea/kinobackend/Theatre/Controller/TheatreController.java
@@ -3,6 +3,7 @@ package gruppe6.kea.kinobackend.Theatre.Controller;
 import gruppe6.kea.kinobackend.Models.Theatre;
 import gruppe6.kea.kinobackend.Theatre.Service.TheatreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/theatres")
 public class TheatreController {
 
--- src/main/java/gruppe6/kea/kinobackend/Ticket/Controller/TicketController.java
+++ src/main/java/gruppe6/kea/kinobackend/Ticket/Controller/TicketController.java
@@ -1,4 +1,7 @@
 package gruppe6.kea.kinobackend.Ticket.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "*")
 public class TicketController {
 }
