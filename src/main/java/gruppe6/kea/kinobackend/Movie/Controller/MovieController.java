a56ca0373b556b36eb9403021c554f5ef144aaad
Nilket <joni0010@stud.kea.dk>
2025-09-30 11:58:31 +0200

    I gang med at lave movie

--- src/main/java/gruppe6/kea/kinobackend/Movie/Controller/MovieController.java
+++ src/main/java/gruppe6/kea/kinobackend/Movie/Controller/MovieController.java
@@ -1,7 +1,31 @@
 package gruppe6.kea.kinobackend.Movie.Controller;
 
+import gruppe6.kea.kinobackend.Models.Movie;
+import gruppe6.kea.kinobackend.Movie.Repository.IMovieRepository;
+import gruppe6.kea.kinobackend.Movie.Service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/movie")
 public class MovieController {
+
+    @Autowired
+    IMovieRepository iMovieRepository;
+    @Autowired
+    MovieService movieService;
+    
+    @GetMapping("/movies")
+    List<Movie> getMovies(){
+        return iMovieRepository.findAll();
+    }
+
+
+
+
 }
--- src/main/java/gruppe6/kea/kinobackend/Movie/Service/MovieService.java
+++ src/main/java/gruppe6/kea/kinobackend/Movie/Service/MovieService.java
@@ -1,4 +1,10 @@
 package gruppe6.kea.kinobackend.Movie.Service;
 
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class MovieService {
+
+    
 }
