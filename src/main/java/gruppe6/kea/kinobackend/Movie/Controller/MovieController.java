4c99b76898265ee3fef32c99139d4c1f6870a930
Nilket <joni0010@stud.kea.dk>
2025-09-30 13:11:23 +0200

    Movie endpoints og service lavet, men fejl i postman

--- src/main/java/gruppe6/kea/kinobackend/Movie/Controller/MovieController.java
+++ src/main/java/gruppe6/kea/kinobackend/Movie/Controller/MovieController.java
@@ -1,28 +1,41 @@
 package gruppe6.kea.kinobackend.Movie.Controller;
 
 import gruppe6.kea.kinobackend.Models.Movie;
-import gruppe6.kea.kinobackend.Movie.Repository.IMovieRepository;
 import gruppe6.kea.kinobackend.Movie.Service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/movie")
+@RequestMapping("/api/movies")
 public class MovieController {
 
     @Autowired
-    IMovieRepository iMovieRepository;
-    @Autowired
     MovieService movieService;
-    
-    @GetMapping("/movies")
-    List<Movie> getMovies(){
-        return iMovieRepository.findAll();
+
+    @GetMapping("")
+    public List<Movie> getMovies(){
+        return movieService.getAllMovies();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getMovieById(@PathVariable int id){
+        return movieService.getMovieById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/post")
+    public Movie createMovie(@RequestBody Movie movie){
+        return movieService.createMovie(movie);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteMovie(@PathVariable int id){
+        movieService.deleteMovie(id);
+        return ResponseEntity.noContent().build();
     }
 
 
