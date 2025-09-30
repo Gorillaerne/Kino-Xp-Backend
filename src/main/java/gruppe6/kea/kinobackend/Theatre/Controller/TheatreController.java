b181a74d2c77b5e7628dd42c3f56cab27787e5ba
Malthe-kea <mati0002@stud.kea.dk>
2025-09-30 10:47:43 +0200

    oprettet metoder mm til getAllTheatres. Mangler at teste

--- src/main/java/gruppe6/kea/kinobackend/Theatre/Controller/TheatreController.java
+++ src/main/java/gruppe6/kea/kinobackend/Theatre/Controller/TheatreController.java
@@ -1,4 +1,14 @@
 package gruppe6.kea.kinobackend.Theatre.Controller;
 
+import gruppe6.kea.kinobackend.Theatre.Service.TheatreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/theatres")
 public class TheatreController {
+
+    @Autowired
+    TheatreService theatreService;
 }
--- src/main/java/gruppe6/kea/kinobackend/Theatre/Service/TheatreService.java
+++ src/main/java/gruppe6/kea/kinobackend/Theatre/Service/TheatreService.java
@@ -1,4 +1,26 @@
 package gruppe6.kea.kinobackend.Theatre.Service;
 
+import gruppe6.kea.kinobackend.Models.Theatre;
+import gruppe6.kea.kinobackend.Theatre.Repository.TheatreRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class TheatreService {
+
+    private final TheatreRepository theatreRepository;
+
+    public TheatreService(RestTemplate restTemplate, TheatreRepository theatreRepository) {
+        this.theatreRepository = theatreRepository;
+    }
+
+    public List<Theatre> getTheatres() {
+    return theatreRepository.findAll();
+    }
+
+
 }
