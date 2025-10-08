78fbbb62c102d9ec893bcece33c4fa05a67aa5fe
Nilket <joni0010@stud.kea.dk>
2025-10-08 09:57:22 +0200

    Tilføjet DTO til at lave biografer og lidt logik

---
+++ src/main/java/gruppe6/kea/kinobackend/DTO/CinemaRequestDTO.java
@@ -0,0 +1,15 @@
+package gruppe6.kea.kinobackend.DTO;
+
+public class CinemaRequestDTO {
+
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+}
