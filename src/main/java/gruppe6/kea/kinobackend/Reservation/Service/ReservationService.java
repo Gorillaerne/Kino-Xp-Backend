3e01218db8e53dee21d8f6cef551c0fe701a4be4
WeberGrillen <oxholmf@gmail.com>
2025-10-09 13:54:54 +0200

    addede import

--- src/main/java/gruppe6/kea/kinobackend/Reservation/Service/ReservationService.java
+++ src/main/java/gruppe6/kea/kinobackend/Reservation/Service/ReservationService.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.io.File;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
